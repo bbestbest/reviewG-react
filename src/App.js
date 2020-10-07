1547d19c602346e73e43adad53de67ad187acd4a
Naratip Supakong <naratipcover@gmail.com>
2020-10-07 21:53:15 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
+import React, {Suspense, lazy} from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './main.css';
 import './reset.css';
 
+const Home = lazy(() => import("./pages/Home"))
+const Action = lazy(() => import("./pages/Action"))
+const Adventure = lazy(() => import("./pages/Adventure"))
+const RPG = lazy(() => import("./pages/RPG"))
+const Simulation = lazy(() => import("./pages/Simulation"))
+const Strategy = lazy(() => import("./pages/Strategy"))
+const Sport = lazy(() => import("./pages/Sport"))
+const SignIn = lazy(() => import("./pages/SignIn"))
+const SignUp = lazy(() => import("./pages/SignUp"))
+
 function App() {
-  return (<>
-    <Header></Header>
-    <Body></Body>
-    <Footer></Footer>
-  </>);
+  return (
+    <>
+    <Router>
+      <Suspense fallback="...loading">
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/action" component={Action} />
+          <Route path="/adventure" component={Adventure} />
+          <Route path="/rpg" component={RPG} />
+          <Route path="/simulation" component={Simulation} />
+          <Route path="/strategy" component={Strategy} />
+          <Route path="/sport" component={Sport} />
+          <Route path="/signIn" component={SignIn} />
+          <Route path="/signUp" component={SignUp} />
+        </Switch>
+      </Suspense>
+    </Router>
+    </>
+  );
 }
 
 export default App;

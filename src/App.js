154c67d177b99b880fb1d70b259a382cf0cca75d
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-08 00:52:41 +0700

    add each pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
+import React , { Suspense , lazy }from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './main.css';
 import './reset.css';
 
+const Home = lazy(() => import("./pages/Home"))
+const Action = lazy(() => import("./pages/Action"))
+const Adventure = lazy(() => import("./pages/Adventure"))
+const RPG = lazy(() => import("./pages/RPG"))
+const Simulation = lazy(() => import("./pages/Simulation"))
+const Strategy = lazy(() => import("./pages/Strategy"))
+const Sport = lazy(() => import("./pages/Sport"))
+
+
 function App() {
   return (<>
-    <Header></Header>
-    <Body></Body>
-    <Footer></Footer>
+    <Router>
+      <Suspense fallback="...loading">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/action" component={Action} />
+          <Route path="/adventure" component={Adventure} />
+          <Route path="/rpg" component={RPG} />
+          <Route path="/Simulation" component={Simulation} />
+          <Route path="/strategy" component={Strategy} />
+          <Route path="/sport" component={Sport} />
+        </Switch>
+      </Suspense>
+    </Router>
   </>);
 }
 
---
+++ src/pages/Action.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Header";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+
+function Action() {
+    return (
+    <>
+        <Navbar></Navbar>
+        <Body></Body>
+        <Footer></Footer>
+    </>
+    ) 
+}
+export default Action;

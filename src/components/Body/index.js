2b801082e22401ed312401a8391fc279a8e9eb1b
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-11 14:58:50 +0700

    change to get from { children }

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import './index.css';
 
-function Body() {
+function Body({children}) {
     return (
     <>
         <div className="body-container">
             <div className="frontCover-container">
-                <div className="item-frontCover">Action</div>
+                <div className="item-frontCover">{children}</div>
             </div>
         </div>
     </>

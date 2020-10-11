65bae723b5c666b24ee62135feae035ecddf0c56
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-11 14:59:37 +0700

    change Header to Navbar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React , { Suspense , lazy }from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './main.css';
-import './reset.css';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './main.css'
+import './reset.css'
 
-const Home = lazy(() => import("./pages/Home"))
-const Action = lazy(() => import("./pages/Action"))
-const Adventure = lazy(() => import("./pages/Adventure"))
-const RPG = lazy(() => import("./pages/RPG"))
-const Simulation = lazy(() => import("./pages/Simulation"))
-const Strategy = lazy(() => import("./pages/Strategy"))
-const Sport = lazy(() => import("./pages/Sport"))
-const Login = lazy(() => import("./pages/Login"))
-const Register = lazy(() => import("./pages/Register"))
+const Home = lazy(() => import('./pages/Home'))
+const Action = lazy(() => import('./pages/Action'))
+const Adventure = lazy(() => import('./pages/Adventure'))
+const RPG = lazy(() => import('./pages/RPG'))
+const Simulation = lazy(() => import('./pages/Simulation'))
+const Strategy = lazy(() => import('./pages/Strategy'))
+const Sport = lazy(() => import('./pages/Sport'))
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
 
-function App() {
+function App () {
   return (
     <>
-    <Router>
-      <Suspense fallback="...loading">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/home" component={Home} />
-          <Route path="/action" component={Action} />
-          <Route path="/adventure" component={Adventure} />
-          <Route path="/rpg" component={RPG} />
-          <Route path="/simulation" component={Simulation} />
-          <Route path="/strategy" component={Strategy} />
-          <Route path="/sport" component={Sport} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </Suspense>
-    </Router>
+      <Router>
+        <Suspense fallback='...loading'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/home' component={Home} />
+            <Route path='/action' component={Action} />
+            <Route path='/adventure' component={Adventure} />
+            <Route path='/rpg' component={RPG} />
+            <Route path='/simulation' component={Simulation} />
+            <Route path='/strategy' component={Strategy} />
+            <Route path='/sport' component={Sport} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </Switch>
+        </Suspense>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainNavbar from './MainNavbar'
+import MinorNavbar from './MinorNavbar'
+
+function Navbar () {
+  return (
+    <>
+      <MainNavbar />
+      <MinorNavbar />
+    </>
+  )
+}
+
+export default Navbar

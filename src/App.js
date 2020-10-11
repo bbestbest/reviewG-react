be355853c7c6af1bf3d771d3018a74d7fe5d67ec
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-12 00:24:47 +0700

    change to routes and use map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Reset } from 'styled-reset'
-
-const Home = lazy(() => import('./pages/Home'))
-const Action = lazy(() => import('./pages/Action'))
-const Adventure = lazy(() => import('./pages/Adventure'))
-const RPG = lazy(() => import('./pages/RPG'))
-const Simulation = lazy(() => import('./pages/Simulation'))
-const Strategy = lazy(() => import('./pages/Strategy'))
-const Sport = lazy(() => import('./pages/Sport'))
-const Login = lazy(() => import('./pages/Login'))
-const Register = lazy(() => import('./pages/Register'))
+import routes from './config/routes'
 
 function App () {
   return (
@@ -19,16 +10,9 @@ function App () {
       <Router>
         <Suspense fallback='...loading'>
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/home' component={Home} />
-            <Route path='/action' component={Action} />
-            <Route path='/adventure' component={Adventure} />
-            <Route path='/rpg' component={RPG} />
-            <Route path='/simulation' component={Simulation} />
-            <Route path='/strategy' component={Strategy} />
-            <Route path='/sport' component={Sport} />
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
+            {Object.keys(routes).map(routeKey => (
+              <Route Key={routeKey} {...routes[routeKey]} />
+            ))}
           </Switch>
         </Suspense>
       </Router>

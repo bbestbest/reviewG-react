65d5848f067697f8699ab62c5932b6dee9ba1bba
Naratip Supakong <naratipcover@gmail.com>
2020-10-13 15:21:59 +0700

    edit Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import MainNavbar from '../components/Navbar/MainNavbar'
-import Body from '../components/Login'
+import Logins from '../components/Login'
 import Footer from '../components/Footer'
 
 function Login () {
   return (
     <>
       <MainNavbar />
-      <Body>Login</Body>
+      <Logins>Login</Logins>
       <Footer />
     </>
   )

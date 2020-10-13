0b6585901c75305a8a67a9a34795fd61a1e10683
Naratip Supakong <naratipcover@gmail.com>
2020-10-13 15:23:55 +0700

    edit Login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -7,7 +8,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: 83vh;
+  height: 89vh;
 `
 
 const LoginContainer = styled.div`
@@ -15,18 +16,85 @@ const LoginContainer = styled.div`
   justify-content: center;
   align-items: center;
   width: 80%;
-  height: 80%;
+  height: 65%;
+`
+const Image = styled.div`
+  /* background-color: #D1C5C2; */
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+const SignInContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: white;
+  border-radius: 2rem;
+  width: 80%;
+  height: 100%;
+  padding: 3rem;
+`
+
+const Head = styled.div`
+  padding: 3rem;
+  font-size: 40px;
+  color: #f04823;
+`
+
+const ButtonInput = styled.input`
+  width: 75%;
+  height: 2rem;
+  border: 2px solid #707070;
+  border-radius: 5px;
+  font-size: 15px;
+  margin: 1rem;
+  padding: 5px 10px 5px 10px;
+  color: #f04823;
+  &::placeholder {
+    color: #f04823;
+  }
+`
+const ButtonSubmit = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 75%;
+  height: 2rem;
+  padding: 5px 10px 5px 10px;
+  margin-top: 3rem;
+  background-color: #f04823;
+  border: 2px solid #f04823;
+  color: white;
+  border-radius: 5px;
+  transition: background-color 0.5s;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  &:hover {
+    background-color: #d04527;
+  }
 `
 
 function Login () {
   return (
     <>
       <Container>
-        <LoginContainer />
+        <LoginContainer>
+          <Image> Image </Image>
+          <SignInContainer>
+            <Head> Login </Head>
+            <ButtonInput placeholder=' Username... ' />
+            <ButtonInput placeholder=' Password... ' />
+            <ButtonSubmit>
+              <Link to='/'>Submit</Link>
+            </ButtonSubmit>
+          </SignInContainer>
+        </LoginContainer>
       </Container>
     </>
   )
 }
-
-export default Login 
+export default Login

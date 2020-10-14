ab7c09950d8d829ffe529c01837ac4ab56a79116
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-15 04:13:55 +0700

    add BackgroundOverlay

--- src/components/BackgroundBlock.js
+++ src/components/BackgroundBlock.js
@@ -3,17 +3,28 @@ import styled from 'styled-components'
 
 const BackgroundImage = styled.div`
   height: ${props => (props.height)}vh;
-  width: auto;
+  width: 100%;
   background-image: url(${props => props.src});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   overflow: hidden;
+  position: relative;
+`
+const BackgroundOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(255,255,255,0) 100%);
 `
 
-function BackgroundBlock ({ src, height = 50, children }) {
+function BackgroundBlock ({ src, height = 80, children, backgroundPosition = 'center', backgroundSize = 'cover' }) {
   return (
-    <BackgroundImage src={src} height={height} />
+    <BackgroundImage src={src} height={height}>
+      <BackgroundOverlay>
+        {children}
+      </BackgroundOverlay>
+    </BackgroundImage>
   )
 }
 

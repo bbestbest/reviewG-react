9cb87c162851553be08c7e4708fa45fdc4cc0405
Naratip Supakong <naratipcover@gmail.com>
2020-10-14 14:38:20 +0700

    change value name Sign import

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import MainNavbar from '../components/Navbar/MainNavbar'
-import Logins from '../components/Login'
+import Sign from '../components/Register'
 import Footer from '../components/Footer'
 
 function Register () {
   return (
     <>
       <MainNavbar />
-      <Logins>Register</Logins>
+      <Sign>Register</Sign>
       <Footer />
     </>
   )

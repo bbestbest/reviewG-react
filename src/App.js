c6d649684cec8b9478d3b9a1ce671488fa87e9a1
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-15 01:34:01 +0700

    add background-color & wrapper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
 import { Reset } from 'styled-reset'
 import routes from './config/routes'
+
+const BackgroundColor = styled.div`
+  background-color: #003d59;
+`
+const Wrapper = styled.div`
+  max-width: 2560px;
+  min-width: 1440px;
+  margin: 0 auto;
+`
 
 function App () {
   return (
@@ -10,9 +20,13 @@ function App () {
       <Router>
         <Suspense fallback='...loading'>
           <Switch>
-            {Object.keys(routes).map(routeKey => (
-              <Route Key={routeKey} {...routes[routeKey]} />
-            ))}
+            <BackgroundColor>
+              <Wrapper>
+                {Object.keys(routes).map(routeKey => (
+                  <Route Key={routeKey} {...routes[routeKey]} />
+                ))}
+              </Wrapper>
+            </BackgroundColor>
           </Switch>
         </Suspense>
       </Router>

402744a715899e2dd73efa364cdec7453d4a7c74
Naratip Supakong <naratipcover@gmail.com>
2020-10-16 00:05:46 +0700

    edit Topic

--- src/components/Topic/index.js
+++ src/components/Topic/index.js
@@ -9,33 +9,96 @@ const Containar = styled.div`
   height: auto;
   padding: 1.5rem 4rem 1.5rem 4rem;
 `
-
 const TopicContainer = styled.div`
-  width: 23vw;
-  height: 20rem;
-  margin: 15px;
-  border: 3px solid ;
+  width: 20vw;
+  height: 19rem;
+  margin: 30px;
+`
+
+const ImageTopic = styled.div`
+  width: auto;
+  height: 11rem;
+  border-radius: 15px;
+  background-color: #99a3a4;
+`
+const Topics = styled.div`
+  font-size: 18px;
+  padding-top: 1rem;
+  width: auto;
+  height: auto;
+`
+const TimeTopics = styled.div`
+  font-size: 12px;
+  padding-top: 10px;
 `
 
 function Topic () {
   return (
     <>
       <Containar>
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
-        <TopicContainer />
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
       </Containar>
     </>
   )

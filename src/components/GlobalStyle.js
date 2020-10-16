ac8784387cbe787cb8aacd2af1b0e66fa763bbf9
Naratip Supakong <naratipcover@gmail.com>
2020-10-16 12:12:32 +0700

    add GlobalStyle

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  /* ${reset}
+
+  html, body, #root {
+    font-family: 'Prompt', sans-serif;
+    scroll-behavior:smooth;
+  }  */
+  
+`
+export default GlobalStyle

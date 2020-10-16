fd8f6109212aa1e93aa4050eeed802499759aaae
Naratip Supakong <naratipcover@gmail.com>
2020-10-16 15:32:18 +0700

    add Topic Categories

---
+++ src/components/Topic/index..js
@@ -0,0 +1,108 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Containar = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: auto;
+  height: auto;
+  padding: 1.5rem 4rem 1.5rem 4rem;
+`
+
+const TopicContainer = styled.div`
+  width: 20vw;
+  height: 19rem;
+  margin: 30px;
+`
+
+const ImageTopic = styled.div`
+  width: auto;
+  height: 11rem;
+  border-radius: 15px;
+  background-color: #99a3a4;
+`
+const Topics = styled.div`
+  font-size: 18px;
+  padding-top: 1rem;
+  width: auto;
+  height: auto;
+`
+const TimeTopics = styled.div`
+  font-size: 12px;
+  padding-top: 10px;
+`
+
+function Topic () {
+  return (
+    <>
+      <Containar>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+        <TopicContainer>
+          <ImageTopic />
+          <Topics>
+            ยอดขาย Among Us ขึ้นแท่นอันดับหนึ่งใน Steam ประจำเดือนกันยายนนี้แล้ว
+          </Topics>
+          <TimeTopics> 16/10/2020 </TimeTopics>
+        </TopicContainer>
+      </Containar>
+    </>
+  )
+}
+
+export default Topic

fbc10fcd39df096f39438885807225d53d268a32
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-18 22:47:08 +0700

    add getData with references

--- src/services/FetchData.js
+++ src/services/FetchData.js
@@ -3,11 +3,20 @@ import axios from 'axios'
 const API_ENDPOINT = 'http://localhost:3333'
 const API_VER = 'api/v1'
 
-export async function getData (table) {
+export async function getData (table, value) {
   return axios(
     {
       method: 'GET',
-      url: `${API_ENDPOINT}/${API_VER}/${table}`
+      url: `${API_ENDPOINT}/${API_VER}/${table}?references=${value}`
+    }
+  ).then(response => response.data.data)
+}
+
+export async function getImage (table, id, value) {
+  return axios(
+    {
+      method: 'GET',
+      url: `${API_ENDPOINT}/${API_VER}/${table}/${id}?references=${value}`
     }
   ).then(response => response.data.data)
 }

94e2b436faaa84ce06e7579f661d98750c27a89d
Naratip Supakong <naratipcover@gmail.com>
2020-10-21 15:10:52 +0700

    Add page AdminPost

---
+++ src/pages/AdminPost.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainNavbar from '../components/Navbar/MainNavbar'
+import Admin from '../components/Admin'
+import Footer from '../components/Footer'
+
+function AdminPost () {
+  return (
+    <>
+      <MainNavbar />
+      <Admin />
+      <Footer />
+    </>
+  )
+}
+
+export default AdminPost

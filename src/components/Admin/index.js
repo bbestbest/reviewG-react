3f76d35f1186e878cceaa14e1e9b9cf92bbce4b1
Naratip Supakong <naratipcover@gmail.com>
2020-10-21 21:39:20 +0700

    move InputPost to folder InputPost.js

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Container, BodyContainer } from '../CatagoriesBody'
+import InputPost from './InputPost'
 import {
   HeadingContainer,
   Heading,
-  PostContainer,
-  SubHeading, ButtonTextarea,
-  ButtonInput,
-  InputFile,
-  ScoreContainer,
-  ScoreCatagories,
-  Score
-} from './AdminStyled.'
+  PostContainer
+} from './AdminStyled'
 
 function Admin () {
   return (
@@ -21,43 +16,7 @@ function Admin () {
             <Heading> POST </Heading>
           </HeadingContainer>
           <PostContainer>
-
-            <SubHeading> Topic </SubHeading>
-            <ButtonTextarea className='Topic ' placeholder=' ex: Topic... ' />
-
-            <SubHeading> Writer </SubHeading>
-            <ButtonInput className='Writer' placeholder=' ex: Writer... ' />
-
-            <SubHeading> Content </SubHeading>
-            <ButtonTextarea
-              className='Content '
-              placeholder=' ex: Content... '
-            />
-
-            <SubHeading> Image </SubHeading>
-            <InputFile className='Image file ' type='file' />
-
-            <SubHeading> Score </SubHeading>
-            <ScoreContainer>
-              <ScoreCatagories className='Story'> Story </ScoreCatagories>
-              <Score type='number' min='0' max='10' />
-            </ScoreContainer>
-
-            <ScoreContainer>
-              <ScoreCatagories className='Gameplay'> Gameplay </ScoreCatagories>
-              <Score type='number' min='0' max='10' />
-            </ScoreContainer>
-
-            <ScoreContainer>
-              <ScoreCatagories className='Performance'> Performance </ScoreCatagories>
-              <Score type='number' min='0' max='10' />
-            </ScoreContainer>
-
-            <ScoreContainer>
-              <ScoreCatagories className='Graphic'> Graphic </ScoreCatagories>
-              <Score type='number' min='0' max='10' />
-            </ScoreContainer>
-
+            <InputPost />
           </PostContainer>
         </BodyContainer>
       </Container>

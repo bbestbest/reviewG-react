4c18426c5a5b78c90f745006d4f47c196371d307
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-22 17:31:16 +0700

    add getDataByCatagories

--- src/services/FetchData.js
+++ src/services/FetchData.js
@@ -3,20 +3,20 @@ import axios from 'axios'
 const API_ENDPOINT = 'http://localhost:3333'
 const API_VER = 'api/v1'
 
-export async function getData (table, value) {
+export async function getData (table, references) {
   return axios(
     {
       method: 'GET',
-      url: `${API_ENDPOINT}/${API_VER}/${table}?references=${value}`
+      url: `${API_ENDPOINT}/${API_VER}/${table}?references=${references}`
     }
   ).then(response => response.data.data)
 }
 
-export async function getImage (table, id, value) {
+export async function getDataByCatagories (table, catagories, references) {
   return axios(
     {
       method: 'GET',
-      url: `${API_ENDPOINT}/${API_VER}/${table}/${id}?references=${value}`
+      url: `${API_ENDPOINT}/${API_VER}/${table}/${catagories}?references=${references}`
     }
   ).then(response => response.data.data)
 }
@@ -26,6 +26,15 @@ export async function getDataById (table, value, catagories, id) {
     {
       method: 'GET',
       url: `${API_ENDPOINT}/${API_VER}/${table}/${catagories}/${id}?references=${value}`
+    }
+  ).then(response => response.data.data)
+}
+
+export async function getImage (table, id, value) {
+  return axios(
+    {
+      method: 'GET',
+      url: `${API_ENDPOINT}/${API_VER}/${table}/${id}?references=${value}`
     }
   ).then(response => response.data.data)
 }

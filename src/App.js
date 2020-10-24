14bbd59d4f8753a44eab9a075ebfc1ef7cf19f1e
Naratip Supakong <naratipcover@gmail.com>
2020-10-24 16:47:52 +0700

    Post to Database of comment and review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
 import styled from 'styled-components'
 import { Reset } from 'styled-reset'
 import GlobalStyle from './components/GlobalStyle'
 import withHelmet from './utils/withHelmet'
+import PostData from './services/PostData'
+import CommentData from './services/CommentData'
 
 const BackgroundColor = styled.div`
   background-color: #003d59;
@@ -16,12 +18,16 @@ const Wrapper = styled.div`
 `
 
 function App () {
+  useEffect(() => {
+    PostData()
+    CommentData()
+  }, [])
   return (
     <>
       <GlobalStyle />
       <Reset />
       <Router>
-        <Suspense fallback='...loading'>
+        <Suspense fallback="...loading">
           <Switch>
             <BackgroundColor>
               <Wrapper>

7d32f4c1cd43b7f3909c65b5956c8faae37393c3
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-10-25 22:16:07 +0700

    add Provider

--- src/components/Navbar/MainNavbar/index.js
+++ src/components/Navbar/MainNavbar/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import AuthContext, { CurrentUser } from '../../../contexts/AuthContext'
+
 // import './index.css'
 
 const Container = styled.div`
@@ -11,8 +13,8 @@ const Container = styled.div`
   height: auto;
   width: auto;
   margin-bottom: auto;
-  padding-right: 2rem;
   position: relative;
+  font-size: 1.2rem;
 `
 const Logo = styled.div`
   display: flex;
@@ -21,7 +23,7 @@ const Logo = styled.div`
   padding-left: 2rem;
   height: auto;
   width: auto;
-  font-size: 20px;
+  font-size: 1.6rem;
   background-color: #f04823;
   text-decoration: none;
   color: white;
@@ -34,13 +36,14 @@ const Logo = styled.div`
 `
 
 const SignButton = styled.div`
+  width: 5%;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  padding: 1rem;
-  /* background: #f04823; */
-  background-color: transparent;
+  padding: 2rem;
+  background: #f04823;
+  /* background-color: transparent; */
   text-decoration: none;
   color: white;
 
@@ -55,23 +58,67 @@ const SignButton = styled.div`
     color: white;
     transition: background-color .8s;
   }
+`
+const UsernameContainer = styled.div`
+  width: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 1rem;
+  background-color: transparent;
+  text-decoration: none;
+`
 
+const LogoutButton = styled.div`
+  width: 5%;
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+  align-items: center;
+  text-align: center;
+  background: #f04823;
+  background-color: transparent;
+  text-decoration: none;
+  color: white;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }  
+    &:hover {
+    /* padding: 10px; */
+    text-decoration: none;
+    background-color: #D04527;
+    color: white;
+    transition: background-color .8s;
+  }
 `
 
 function MainNavbar () {
+  // const [isLogin] = useCurrentUser()
+  // const [username] = useCurrentUser()
+  const { isLogin, setIsLogin, globalUsername, setGlobalUsername } = useContext(AuthContext)
+
+  // useEffect(() => {
+  //   setIsLogin(true)
+  // })
+
+  const handleOnLogout = () => {
+    setIsLogin(false)
+    setGlobalUsername(null)
+  }
+
   return (
     <>
-      <Container>
-        <Logo>
-          <Link to='/'>review-G</Link>
-        </Logo>
-        <SignButton>
-          <Link to='/register'>Sign Up</Link>
-        </SignButton>
-        <SignButton>
-          <Link to='/login'>Sign In</Link>
-        </SignButton>
-      </Container>
+      <CurrentUser>
+        <Container>
+          <Logo>
+            <Link to='/'>review-G</Link>
+          </Logo>
+          {/* <SignButton><Link to='/register'>Sign Up</Link></SignButton><SignButton><Link to='/login'>Sign In</Link></SignButton> */}
+          {!isLogin ? (<><SignButton><Link to='/register'>Sign Up</Link></SignButton><SignButton><Link to='/login'>Sign In</Link></SignButton></>) : <><UsernameContainer>{globalUsername}</UsernameContainer><LogoutButton onClick={handleOnLogout}>Logout</LogoutButton></>}
+        </Container>
+      </CurrentUser>
     </>
   )
 }
